d42db2f08a0c7c660f99a146f0693b41b1924f41
Myoung Chan, Kim <89979849+mingtian-chan@users.noreply.github.com>
2023-09-03 02:40:57 +0900

    Delete movie/movie directory

--- movie/movie/src/main/java/chan/movie/movie/step02/DiscountCondition.java
+++
@@ -1,5 +0,0 @@
-package chan.movie.movie.step02;
-
-public interface DiscountCondition {
-    boolean isSatisfiedBy(Screening screening);
-}
--- movie/movie/src/main/java/chan/movie/movie/step02/DiscountPolicy.java
+++
@@ -1,7 +0,0 @@
-package chan.movie.movie.step02;
-
-import chan.movie.money.Money;
-
-public interface DiscountPolicy {
-    Money calculateDiscountAmount(Screening screening);
-}
